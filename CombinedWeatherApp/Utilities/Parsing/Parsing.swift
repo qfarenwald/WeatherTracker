174589015fbee6b590004a9255dda7ee127e871c
Quinne Farenwald <quinne.farenwald@MacBook-Pro.local>
2020-02-10 14:22:40 -0700

    Finish fetching data setup

--- CombinedWeatherApp/Utilities/Parsing/Parsing.swift
+++ CombinedWeatherApp/Utilities/Parsing/Parsing.swift
@@ -26,3 +26,17 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
+import Foundation
+import Combine
+
+func decode<T: Decodable>(_ data: Data) -> AnyPublisher<T, WeatherError> {
+  let decoder = JSONDecoder()
+  decoder.dateDecodingStrategy = .secondsSince1970
+
+  return Just(data)
+    .decode(type: T.self, decoder: decoder)
+    .mapError { error in
+      .parsing(description: error.localizedDescription)
+    }
+    .eraseToAnyPublisher()
+}
